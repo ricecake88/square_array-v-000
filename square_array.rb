a7a54ec2df40620de993a579ca8ea388bf93fdf5
Grace Shih <vocalise@gmail.com>
2018-05-05 07:08:02 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,10 @@
 def square_array(array)
- # newArray = []
- # array.each do |item|
- #    newArray.push(item**2)
- #  end
- #  return newArray
- array.collect {|n| n*n}
+  newArray = []
+  array.each do |item|
+     newArray.push(item**2)
+   end
+   return newArray
+ #array.collect {|n| n*n}
 end
 
 woo = [3, 4, 3]
