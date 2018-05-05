8d3121b95dd351ae8bdb3c4010c8fc3e604f07a7
Grace Shih <vocalise@gmail.com>
2018-05-05 06:53:25 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,9 @@
 def square_array(array)
-  newArray = []
-  array.each do |item|
-    newArray.push(item**2)
-  end
-  return newArray
+ # newArray = []
+ # array.each do |item|
+ #    newArray.push(item**2)
+ #  end
+ #  return newArray
+ array.collect do |item|
+   item**2
 end
